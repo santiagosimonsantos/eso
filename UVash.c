9f6e7effd9844602516c5b2c039ab38902be57c9
Santiago Simon <santiago.simon.santos.dev@gmail.com>
2024-02-27 13:57:19 +0100

    feat: struct

--- UVash.c
+++ UVash.c
@@ -7,6 +7,14 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
+typedef struct
+{
+    char *name;
+    char *args[100];
+    char *redirections[25];
+    int numberOfRedirections;
+} command;
+
 int errorMessage()
 {
     char error_message[30] = "An error has occurred\n";
@@ -41,20 +49,47 @@ int executeChdir(char *args[])
     return 0;
 }
 
-int executeStandardCommand(char *args[], char *redirection, int mode)
+int executeStandardCommand(char *args[], char *redirections[], int mode)
 {
     int stream;
     pid_t pid;
-    stream = open(redirection, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    char *found;
+    char *fileName;
+    int onlyOne = 0;
+    while ((found = strsep(&redirections[1], " \t")) != NULL)
+    {
+        if (*found != '\0')
+        {
+            if (onlyOne == 1 && mode > 1)
+            {
+                errorMessage();
+                return 1;
+            }
+            onlyOne = 1;
+            fileName = found;
+        }
+    }
+    stream = open(fileName, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
     pid = fork();
+
     if (pid == 0)
     {
         if (mode > 1)
         {
+
+            if (onlyOne == 0)
+            {
+                errorMessage();
+                return 1;
+            }
             dup2(stream, 1);
             dup2(stream, 2);
         }
-        execvp(args[0], args);
+        if (execvp(args[0], args) == -1)
+        {
+            errorMessage();
+            return 1;
+        }
     }
     else
     {
@@ -63,7 +98,27 @@ int executeStandardCommand(char *args[], char *redirection, int mode)
     return 0;
 }
 
-int parseCommand(char *subString)
+int split(char *string, char *delim, char *array[], int arraySize)
+{
+    char *found;
+    int i = 0;
+    int size = 0;
+    while ((found = strsep(&string, delim)) != NULL)
+    {
+        if (*found != '\0')
+        {
+            array[i++] = found;
+        }
+        size++;
+    }
+    for (int k = i; k < arraySize; k++)
+    {
+        array[k] = NULL;
+    }
+    return size;
+}
+
+int parseCommand(char *subString, command *command)
 {
     char *redirections[25];
     char *found;
@@ -71,66 +126,64 @@ int parseCommand(char *subString)
     int mode = 0;
     char *args[100];
     int j = 0;
-    char *delim = " \t";
-    while ((found = strsep(&subString, ">")) != NULL)
+    mode = split(subString, ">", &redirections, 25);
+    split(redirections[0], " \t", &args, 100);
+    command->name = args[0];
+    for (int k = 0; k < 100; k++)
     {
-        if (*found != '\0')
-        {
-            redirections[i++] = found;
-            mode++;
-        }
+        command->args[k] = args[k];
     }
-    for (int k = i; k < 25; k++)
+    for (int k = 0; k < 25; k++)
     {
-        redirections[k] = NULL;
+        command->redirections[k] = redirections[k];
     }
-    while ((found = strsep(&redirections[0], delim)) != NULL)
-    {
-        if (*found != '\0')
-        {
-            args[j++] = found;
-        }
-    }
-    for (int k = j; k < 100; k++)
-    {
-        args[k] = NULL;
-    }
-    for (int k = 1; k < mode; k++)
-    {
-        while ((found = strsep(&redirections[k], delim)) != NULL)
-        {
-            if (*found != '\0')
-            {
-                redirections[k] = found;
-                break;
-            }
-        }
-    }
-    if (strcmp(args[0], "exit") == 0)
-    {
-        exit(0);
-    }
-    else if (strcmp(args[0], "cd") == 0)
-    {
-        executeChdir(args);
-    }
-    else
-    {
-        executeStandardCommand(args, redirections[1], mode);
-
-    }
+    command->numberOfRedirections = mode;
     return 0;
 }
 
 int parseLine(char *line)
 {
+    line = strsep(&line, "\n");
+    command commands[100];
     char *found;
     char *delim = "&";
+    int i = 0;
+    char *redirections[25];
+    int mode = 0;
+    char *args[100];
     while ((found = strsep(&line, delim)) != NULL)
     {
         if (*found != '\0')
         {
-            parseCommand(found);
+            parseCommand(found, &commands[i++]);
+            args[0] = commands[i - 1].name;
+            for (int k = 0; k < 100; k++)
+            {
+                args[k] = commands[i - 1].args[k];
+            }
+            for (int k = 0; k < 25; k++)
+            {
+                redirections[k] = commands[i - 1].redirections[k];
+            }
+            mode = commands[i - 1].numberOfRedirections;
+            if (strcmp(args[0], "exit") == 0)
+            {
+                if (args[1] != NULL)
+                {
+                    errorMessage();
+                    return 1;
+                }
+                exit(0);
+            }
+            else if (strcmp(args[0], "cd") == 0)
+            {
+                executeChdir(args);
+            }
+            else
+            {
+
+                executeStandardCommand(args, redirections, mode);
+            }
         }
     }
     return 0;
@@ -165,7 +218,7 @@ int interactiveMode()
 int batchMode(char *filePath)
 {
     FILE *stream;
-    char **line = NULL;
+    char *line = NULL;
     size_t len = 0;
     ssize_t nread;
     stream = fopen(filePath, "r");
@@ -176,7 +229,6 @@ int batchMode(char *filePath)
     }
     while ((nread = getline(&line, &len, stream)) != -1)
     {
-        line[strlen(line) - 1] = '\0';
         parseLine(line);
     }
     if (!feof(stream))
